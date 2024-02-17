9f3c015d0d9188e1c01d9748de8489d6f486d904
Steglasaurous <me@steglasaurous.com>
2024-02-17 14:45:18 -0500

    Fix up docker info

--- docker/irlcontrol/run.sh
+++ docker/irlcontrol/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Read the ENV file and make appropriate config substitutions prior to running.
 # twitch channel
-envsubst < /server/config.dist.json > /server/config.json
+# envsubst < /server/config.dist.json > /server/config.json
+envsubst < /config.json > /server/config.json
 cd /server && npm run start
