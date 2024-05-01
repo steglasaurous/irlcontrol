b1f4223f5bbbce3e0af0ac0dc14cf11f0c1885df
steglasaurous <me@steglasaurous.com>
2024-05-01 08:28:43 -0400

    WIP

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
   wsUrl: 'https://irl.steglasaurous.com',
+  videoFeedbackSource: 'https://steglasaurous.com/videotest/stream_0.m3u8',
   // rtirlPullKey: '',
   // twitchUsername: ''
 };
