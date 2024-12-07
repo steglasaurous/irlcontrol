92fff6356b66ac6142a0e399d7841ca4a1cd2c53
Scott Teglasi <scott.teglasi@experiencepoint.com>
2024-12-06 23:22:08 -0500

    Add server stuff to emit irl stats

--- server/src/modules/irl-stats/services/irl-stats.service.ts
+++ server/src/modules/irl-stats/services/irl-stats.service.ts
@@ -8,10 +8,12 @@ import { Cron, CronExpression } from '@nestjs/schedule';
 export class IrlStatsService {
     irlStat: IrlStats;
     irlUpdates$: Subject<IrlStats> = new Subject<IrlStats>();
+    lastUpdatedAt: number = 0;
 
     private running: boolean = true; // FIXME: Change to default to false and have some sort of trigger to turn it on?
     // Store as CSV so it's easy to append.  Don't have to worry about JSON structures, etc.
 
+    private totalDistanceKm: number = 0;
     constructor(
         @Inject('RTIRL_PULL_KEY') private rtirlPullKey: string,
         private readonly httpService: HttpService,
@@ -37,12 +39,74 @@ export class IrlStatsService {
                     `https://rtirl.com/api/pull?key=${this.rtirlPullKey}`,
                 ),
             );
+            if (this.lastUpdatedAt == response.data.updatedAt) {
+                // No change, don't update.
+                return;
+            }
+            this.lastUpdatedAt = response.data.updatedAt;
+
+            let speedKph = (response.data.speed * 3.6) | 0;
+            if (speedKph < 1) {
+                speedKph = 0;
+            }
+            const speedMph = speedKph * 0.621371;
+
+            if (this.irlStat !== undefined) {
+                const delta = this.distanceInKmBetweenEarthCoordinates(
+                    response.data.location.latitude,
+                    response.data.location.longitude,
+                    this.irlStat.latitude,
+                    this.irlStat.longitude,
+                );
+
+                this.totalDistanceKm = this.totalDistanceKm + delta;
+            }
 
             this.irlStat = {
                 latitude: response.data.location.latitude,
                 longitude: response.data.location.longitude,
+                distanceKm: this.totalDistanceKm,
+                distanceMiles: this.totalDistanceKm * 0.621371,
+                speedKph: speedKph,
+                speedMph: speedMph,
             };
+
+            this.irlUpdates$.next(this.irlStat);
+
+            // Note that because of GPS drift, different gps points will keep comming even if
+            // the subject is stationary. Each new gps point will be considered as subject is moving
+            // and it will get added to the total distance. Each addition will be tiny but it will
+            // addup over time and can become visible. So, at the end the shown distance might look
+            // slightly more than expected.
         }
+    }
+
+    private degreesToRadians(degrees: number): number {
+        return (degrees * Math.PI) / 180;
+    }
+
+    private distanceInKmBetweenEarthCoordinates(
+        lat1: number,
+        lon1: number,
+        lat2: number,
+        lon2: number,
+    ) {
+        const earthRadiusKm = 6371;
+
+        const dLat = this.degreesToRadians(lat2 - lat1);
+        const dLon = this.degreesToRadians(lon2 - lon1);
+
+        lat1 = this.degreesToRadians(lat1);
+        lat2 = this.degreesToRadians(lat2);
+
+        const a =
+            Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+            Math.sin(dLon / 2) *
+                Math.sin(dLon / 2) *
+                Math.cos(lat1) *
+                Math.cos(lat2);
+        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        return earthRadiusKm * c;
     }
 }
 
--- server/src/modules/irl-stats/services/irl-stats.ts
+++ server/src/modules/irl-stats/services/irl-stats.ts
@@ -1,4 +1,8 @@
 export interface IrlStats {
     latitude: number;
     longitude: number;
+    distanceKm: number;
+    distanceMiles: number;
+    speedKph: number;
+    speedMph: number;
 }
